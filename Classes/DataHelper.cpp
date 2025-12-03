6c9f4eefb369c721abdfbc23f0d9d9131baf8cf9
crane-fog <13901908918@163.com>
2025-12-03 14:05:35 +0800

    feat: basic architecture show

--- Classes/DataHelper.cpp
+++ Classes/DataHelper.cpp
@@ -1,72 +1,13 @@
 #include <fstream>
-#include <vector>
 #include "DataHelper.h"
 #include "ArchInfo.h"
 // todo：改用cocos2d::FileUtils
 
-bool DataHelper::readArchData(const std::string& file_name, unsigned long long& time, ArchStatus target[MAP_SIZE][MAP_SIZE])
+void DataHelper::mapToList(const ArchStatus source[MAP_SIZE][MAP_SIZE], std::vector<ArchData>& target)
 {
-    unsigned char x = 0, y = 0;
-    unsigned short num = 0;
-    ArchData* buffer = nullptr;
-    std::ifstream infile(file_name, std::ios::binary);
-    if (!infile) {
-        return false;
-    }
-
-    infile.read(reinterpret_cast<char*>(&time), sizeof(unsigned long long));
-    infile.read(reinterpret_cast<char*>(&num), sizeof(unsigned short));
-
-    buffer = new ArchData[num];
-    infile.read(reinterpret_cast<char*>(buffer), sizeof(ArchData) * num);
-    
-    for (int i = 0; i < MAP_SIZE; i++) {
-        for (int j = 0; j < MAP_SIZE; j++) {
-            target[i][j].no_ = INVALID_ARCH_NO;
-        }
-    }
-
-    for (unsigned short i = 0; i < num; i++) {
-        x = buffer[i].x_;
-        y = buffer[i].y_;
-        target[x][y].no_ = buffer[i].no_;
-        target[x][y].level_ = buffer[i].level_;
-        target[x][y].current_hp_ = kArchInfo.at(buffer[i].no_)[buffer[i].level_ - 1].hp_;
-        target[x][y].current_capacity_ = buffer[i].current_capacity_;
-        // 填充建筑占地
-        for (unsigned char dx = 0; dx < kArchInfo.at(buffer[i].no_)[buffer[i].level_ - 1].size_; dx++) {
-            for (unsigned char dy = 0; dy < kArchInfo.at(buffer[i].no_)[buffer[i].level_ - 1].size_; dy++) {
-                if (!(dx == 0 && dy == 0)) {
-                    if (x + dx >= MAP_SIZE || y + dy >= MAP_SIZE) {
-                        delete[] buffer;
-                        return false;
-                    }
-                    target[x + dx][y + dy] = target[x][y];
-                }
-            }
-        }
-    }
-
-    delete[] buffer;
-    infile.close();
-    return true;
-}
-
-bool DataHelper::writeArchData(const std::string& file_name, unsigned long long time, const ArchStatus source[MAP_SIZE][MAP_SIZE])
-{
-    unsigned short num = 0;
     unsigned char size = 0;
-    std::vector<ArchData> data;
     ArchData temp;
-    std::ofstream outfile(file_name, std::ios::binary);
-    if (!outfile) {
-        return false;
-    }
-
-    outfile.write(reinterpret_cast<const char*>(&time), sizeof(unsigned long long));
-
     bool visited[MAP_SIZE][MAP_SIZE] = { false };
-
     for (int x = 0; x < MAP_SIZE; x++) {
         for (int y = 0; y < MAP_SIZE; y++) {
             if (visited[x][y]) {
@@ -85,7 +26,7 @@ bool DataHelper::writeArchData(const std::string& file_name, unsigned long long 
             temp.x_ = static_cast<unsigned char>(x);
             temp.y_ = static_cast<unsigned char>(y);
             temp.current_capacity_ = current.current_capacity_;
-            data.push_back(temp);
+            target.push_back(temp);
 
             size = kArchInfo.at(current.no_)[current.level_ - 1].size_;
 
@@ -98,6 +39,69 @@ bool DataHelper::writeArchData(const std::string& file_name, unsigned long long 
             }
         }
     }
+}
+
+void DataHelper::listToMap(const std::vector<ArchData>& source, ArchStatus target[MAP_SIZE][MAP_SIZE])
+{
+    unsigned char size = 0, x = 0, y = 0;
+    for (int i = 0; i < MAP_SIZE; i++) {
+        for (int j = 0; j < MAP_SIZE; j++) {
+            target[i][j].no_ = INVALID_ARCH_NO;
+        }
+    }
+
+    for (unsigned short i = 0; i < source.size(); i++) {
+        x = source[i].x_;
+        y = source[i].y_;
+        target[x][y].no_ = source[i].no_;
+        target[x][y].level_ = source[i].level_;
+        target[x][y].current_hp_ = kArchInfo.at(source[i].no_)[source[i].level_ - 1].hp_;
+        target[x][y].current_capacity_ = source[i].current_capacity_;
+        // 填充建筑占地
+        for (unsigned char dx = 0; dx < kArchInfo.at(source[i].no_)[source[i].level_ - 1].size_; dx++) {
+            for (unsigned char dy = 0; dy < kArchInfo.at(source[i].no_)[source[i].level_ - 1].size_; dy++) {
+                if (!(dx == 0 && dy == 0)) {
+                    target[x + dx][y + dy] = target[x][y];
+                }
+            }
+        }
+    }
+}
+
+bool DataHelper::readArchData(const std::string& file_name, time_t& time, ArchStatus target[MAP_SIZE][MAP_SIZE])
+{
+    unsigned char x = 0, y = 0;
+    unsigned short num = 0;
+    std::vector<ArchData> buffer;
+    std::ifstream infile(file_name, std::ios::binary);
+    if (!infile) {
+        return false;
+    }
+
+    infile.read(reinterpret_cast<char*>(&time), sizeof(unsigned long long));
+    infile.read(reinterpret_cast<char*>(&num), sizeof(unsigned short));
+
+    buffer.resize(num);
+    infile.read(reinterpret_cast<char*>(buffer.data()), sizeof(ArchData) * num);
+
+    listToMap(buffer, target);
+
+    infile.close();
+    return true;
+}
+
+bool DataHelper::writeArchData(const std::string& file_name, time_t time, const ArchStatus source[MAP_SIZE][MAP_SIZE])
+{
+    unsigned short num = 0;
+    std::vector<ArchData> data;
+    std::ofstream outfile(file_name, std::ios::binary);
+    if (!outfile) {
+        return false;
+    }
+
+    outfile.write(reinterpret_cast<const char*>(&time), sizeof(unsigned long long));
+
+    mapToList(source, data);
 
     num = static_cast<unsigned short>(data.size());
     outfile.write(reinterpret_cast<const char*>(&num), sizeof(unsigned short));
--- Classes/DataHelper.h
+++ Classes/DataHelper.h
@@ -2,34 +2,26 @@
 #define __DATA_HELPER_H__
 
 #include <string>
+#include <vector>
 #include "Arch.h"
 #include "BaseMap.h"
 
-const std::string kMainVillageDataFile = "data/MainVillageData.dat";
-
-// 用于读取建筑数据
-struct ArchData {
-    typedef unsigned int UI;
-    typedef unsigned char UC;
-    // 建筑种类的编号
-    UC no_;
-    // 建筑等级
-    UC level_;
-    // 建筑位置
-    UC x_;
-    UC y_;
-
-    // 资源建筑
-    // 当前容量
-    UI current_capacity_;
-};
+const std::string kMainVillageDataFile = "../data/MainVillageData.dat";
 
 // 数据文件操作
 class DataHelper {
 public:
+    // [44][44]的地图表形式与一个list形式的转换
+    // 建筑地图表转换为建筑列表（需要保证target为空）
+    static void mapToList(const ArchStatus source[MAP_SIZE][MAP_SIZE], std::vector<ArchData>& target);
+
+    // 建筑列表转换为建筑地图表（需要保证source有内容）
+    static void listToMap(const std::vector<ArchData>& source, ArchStatus target[MAP_SIZE][MAP_SIZE]);
+
     // 读数据文件
-    static bool readArchData(const std::string& file_name, unsigned long long& time, ArchStatus target[MAP_SIZE][MAP_SIZE]);
+    static bool readArchData(const std::string& file_name, time_t& time, ArchStatus target[MAP_SIZE][MAP_SIZE]);
+
     // 写数据文件
-    static bool writeArchData(const std::string& file_name, unsigned long long time, const ArchStatus source[MAP_SIZE][MAP_SIZE]);
+    static bool writeArchData(const std::string& file_name, time_t time, const ArchStatus source[MAP_SIZE][MAP_SIZE]);
 };
 #endif // __DATA_HELPER_H__
