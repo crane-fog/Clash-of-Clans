b37e675f7dc0e7cb4f52dda19143b5b6a271410f
Julia <2621309300@qq.com>
2025-12-23 04:59:38 +0800

    feat: WallBreaker初步

---
+++ Classes/WallBreaker.cpp
@@ -0,0 +1,50 @@
+#include "WallBreaker.h"
+#include "TroopTargetManager.h"
+#include "AudioEngine.h"
+
+WallBreaker::WallBreaker(BaseMap* base_map, int level, cocos2d::Vec2 position)
+    : Troop(base_map, level, position, WALLT, MELEE_AOE_GROUND, 2, 3, 1.8f, 1.0f, 0.4f,
+        std::array<float, MAX_TROOP_LEVEL + 1>({0,12,16,20,24,30}),
+        std::array<float, MAX_TROOP_LEVEL + 1>({0,20,23,26,30,36}),
+        std::array<int, MAX_TROOP_LEVEL + 1>({0,0,10000,50000,130000,300000}),
+        std::array<float, MAX_TROOP_LEVEL + 1>({0,0,0.5,1,2,4}),
+        std::array<uchar, MAX_TROOP_LEVEL + 1>({0,0,2,4,5,6})
+        )
+{
+}
+
+WallBreaker* WallBreaker::create(BaseMap* base_map, int level, cocos2d::Vec2 position) {
+    WallBreaker* pRet = new(std::nothrow) WallBreaker(base_map,level,position);
+    if (pRet && pRet->initWithFile(pics_wallbreaker.at(level))) {
+        pRet->autorelease();
+        return pRet;
+    }
+    else {
+        delete pRet; pRet = nullptr; return nullptr;
+    }
+}
+
+bool WallBreaker::initWithFile(const std::string& filename) {
+    if (!Troop::initWithFile(filename)) {
+        return false;
+    }
+    //base_map_->sprites_.push_back(this);
+    return true;
+}
+
+void WallBreaker::performAttack() {
+    // 执行近战范围攻击
+    
+    // 播放攻击动画
+
+    //// 播放攻击音效
+    //int wallbreaker_hit = cocos2d::AudioEngine::play2d("music/babarian_hit.mp3",false,0.7f);
+    //// 检查音频的状态，直到播放完成
+    //this->schedule([wallbreaker_hit, this](float dt) {
+    //    if (cocos2d::AudioEngine::getState(wallbreaker_hit) == cocos2d::AudioEngine::AudioState::PAUSED) {
+    //        // 停止音效播放并释放资源
+    //        cocos2d::AudioEngine::uncache("music/babarian_hit.mp3");
+    //        this->unschedule("stop_audio_key"); // 停止检查
+    //    }
+    //    }, 0.1f, "stop_audio_key");
+}
---
+++ Classes/WallBreaker.h
@@ -0,0 +1,42 @@
+#ifndef __WALLBREAKER_H__
+#define __WALLBREAKER_H__
+#include<vector>
+#include<string>
+#include "Troop.h"
+
+const std::vector<std::string> pics_wallbreaker = {
+    "troop/Wall_Breaker1-2.webp",
+    "troop/Wall_Breaker1-2.webp",
+    "troop/Wall_Breaker1-2.webp",
+    "troop/Wall_Breaker3-4.webp",
+    "troop/Wall_Breaker3-4.webp",
+    "troop/Wall_Breaker5.webp",
+};
+
+class WallBreaker : public Troop {
+private:
+
+public:
+    // 构造函数
+    WallBreaker(BaseMap* base_map, int level = 1, cocos2d::Vec2 position = cocos2d::Vec2::ZERO);
+
+    // 静态创建函数
+    static WallBreaker* create(BaseMap* base_map, int level = 1, cocos2d::Vec2 position = cocos2d::Vec2::ZERO);
+
+    // 初始化
+    virtual bool initWithFile(const std::string& filename) override;
+
+    // 执行攻击
+    virtual void performAttack() override;
+
+    // 获取士兵类型（地面兵种）
+    virtual ArchTargetType getTargetType() const override { return GROUND; }
+
+    //获取当前位置-像素坐标，子类需要重写来保证视觉上中心在需要的坐标
+    virtual cocos2d::Vec2 getPixelPosition() const override{ return CoordAdaptor::cellToPixel(base_map_, cocos2d::Vec2(position_.x+0.1, position_.y-0.3)); }
+
+    // 获取士兵类型索引（用于区分不同子类类型）
+    virtual TroopType getTroopTypeIndex() const override { return WALL_BREAKER; }
+};
+
+#endif // __WALLBREAKER_H__
